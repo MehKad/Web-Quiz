9b21a12aac0b7c9b1b6f1b8fe8da1d18cb1b7659
MehKad <mehkadiri@gmail.com>
2024-01-13 20:51:35 +0000

    added toast notification after added quiz

--- src/AddQuiz.js
+++ src/AddQuiz.js
@@ -2,8 +2,13 @@ import React, { useState, useContext } from "react";
 import { UserContext } from "./userContext";
 import axios from "axios";
 import "./style/AddQuiz.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useNavigate } from "react-router-dom";
 
 const AddQuiz = () => {
+  const navigate = useNavigate();
+
   const { user, setUser } = useContext(UserContext);
   const [quiz, setQuiz] = useState({
     title: "",
@@ -38,81 +43,86 @@ const AddQuiz = () => {
     try {
       const response = await axios.post("http://localhost:5000/api/add", quiz);
       console.log(response.data);
+      toast.success("Added successful!");
+      navigate("/home");
     } catch (error) {
       console.error(error);
     }
   };
 
   return (
-    <div className="add-quiz-details">
-      <h1 className="quiz-title">Quiz Details</h1>
-      <form onSubmit={handleSubmit} className="form-container">
-        <label className="label-title">Quiz Title:</label>
-        <input
-          type="text"
-          value={quiz.title}
-          onChange={(e) => setQuiz({ ...quiz, title: e.target.value })}
-          className="input-field"
-        />
-        {quiz.questions.map((question, qIndex) => (
-          <div key={qIndex} className="question-container">
-            <label className="question-label">Question:</label>
-            <input
-              type="text"
-              value={question.text}
-              onChange={(e) => {
-                const newQuestions = [...quiz.questions];
-                newQuestions[qIndex].text = e.target.value;
-                setQuiz({ ...quiz, questions: newQuestions });
-              }}
-              className="input-field"
-            />
-            {question.options.map((option, oIndex) => (
-              <div key={oIndex} className="option-container">
-                <label className="option-label">Option:</label>
-                <input
-                  type="text"
-                  value={option}
-                  onChange={(e) => {
-                    const newQuestions = [...quiz.questions];
-                    newQuestions[qIndex].options[oIndex] = e.target.value;
-                    setQuiz({ ...quiz, questions: newQuestions });
-                  }}
-                  className="input-field"
-                />
-              </div>
-            ))}
-            <label className="question-label">Correct Option:</label>
-            <input
-              type="text"
-              value={question.correctOption}
-              onChange={(e) => {
-                const newQuestions = [...quiz.questions];
-                newQuestions[qIndex].correctOption = e.target.value;
-                setQuiz({ ...quiz, questions: newQuestions });
-              }}
-              className="input-field"
-            />
-            <button
-              type="button"
-              onClick={() => addOption(qIndex)}
-              className="add-button"
-            >
-              Add Option
+    <>
+      <div className="add-quiz-details">
+        <h1 className="quiz-title">Quiz Details</h1>
+        <form onSubmit={handleSubmit} className="form-container">
+          <label className="label-title">Quiz Title:</label>
+          <input
+            type="text"
+            value={quiz.title}
+            onChange={(e) => setQuiz({ ...quiz, title: e.target.value })}
+            className="input-field"
+          />
+          {quiz.questions.map((question, qIndex) => (
+            <div key={qIndex} className="question-container">
+              <label className="question-label">Question:</label>
+              <input
+                type="text"
+                value={question.text}
+                onChange={(e) => {
+                  const newQuestions = [...quiz.questions];
+                  newQuestions[qIndex].text = e.target.value;
+                  setQuiz({ ...quiz, questions: newQuestions });
+                }}
+                className="input-field"
+              />
+              {question.options.map((option, oIndex) => (
+                <div key={oIndex} className="option-container">
+                  <label className="option-label">Option:</label>
+                  <input
+                    type="text"
+                    value={option}
+                    onChange={(e) => {
+                      const newQuestions = [...quiz.questions];
+                      newQuestions[qIndex].options[oIndex] = e.target.value;
+                      setQuiz({ ...quiz, questions: newQuestions });
+                    }}
+                    className="input-field"
+                  />
+                </div>
+              ))}
+              <label className="question-label">Correct Option:</label>
+              <input
+                type="text"
+                value={question.correctOption}
+                onChange={(e) => {
+                  const newQuestions = [...quiz.questions];
+                  newQuestions[qIndex].correctOption = e.target.value;
+                  setQuiz({ ...quiz, questions: newQuestions });
+                }}
+                className="input-field"
+              />
+              <button
+                type="button"
+                onClick={() => addOption(qIndex)}
+                className="add-button"
+              >
+                Add Option
+              </button>
+            </div>
+          ))}
+
+          <div className="buttons-wrapper">
+            <button type="button" onClick={addQuestion} className="add-button">
+              Add Question
+            </button>
+            <button type="submit" className="submit-button">
+              Submit
             </button>
           </div>
-        ))}
-
-        <div className="buttons-wrapper">
-          <button type="button" onClick={addQuestion} className="add-button">
-            Add Question
-          </button>
-          <button type="submit" className="submit-button">
-            Submit
-          </button>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 };
 
